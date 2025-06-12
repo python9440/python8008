226efcde225298e69200de2acebf1a13ef7ca2b9
python9440 <kummethabharathi93@gmail.com>
2025-06-11 20:59:19 -1200

    Add files via upload

---
+++ program1.6.py
@@ -0,0 +1,6 @@
+l=eval(input("enter the length of the rectangle:"))
+b=eval(input("enter the breadth of the rectangle:"))
+area=l+b
+perimeter=2*(l+b)
+print("area of the rectangle is:",area)
+print("perimeter of the rectangle is:",perimeter)
