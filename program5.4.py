c683af8f930df68310b7e2436e70c4101e5892ce
python9440 <kummethabharathi93@gmail.com>
2025-06-11 21:09:24 -1200

    Add files via upload

---
+++ program5.4.py
@@ -0,0 +1,8 @@
+class book:
+    def _init_(self,title):
+        self.title=title
+    def create_book_list():
+        return[book("python 101"),book("ai basics"),book("data science")]
+    books=create_book-list()
+    for b in books:
+        print("book title:",b.title)
