786eb1ad3944dca3b277ee4fc0eccf8083547bf5
python9440 <kummethabharathi93@gmail.com>
2025-06-11 21:08:37 -1200

    Add files via upload

---
+++ program5.2.py
@@ -0,0 +1,8 @@
+class student:
+    def_init_(self,name,marks):
+        self.name=name
+        self.marks=marks
+def print_student_info(student):
+    print(f"name:{student.name},marks:{stucent.marks}")
+s1=student("tillu",92)
+s1.print_student_info()
