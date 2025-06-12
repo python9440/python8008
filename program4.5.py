85425e6efe01a797dacfb1066f4211859194c02b
python9440 <kummethabharathi93@gmail.com>
2025-06-11 21:07:24 -1200

    Add files via upload

---
+++ program4.5.py
@@ -0,0 +1,12 @@
+def calculate_sum(numbers):
+    total=0
+for num in numbers:
+    total+=num
+    return total
+list=[]
+n=list(input("enter the number of elements in the list:"))
+for i in range(n):
+    num=eval(input("enter elements{}:",format(i+1)))
+list.append(num)
+result=calculate_sum(list)
+print("the sum of the list is",result)
