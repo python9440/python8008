56d2cb8a0bbdb6c8ef8851c4e8c85a46b1181497
python9440 <kummethabharathi93@gmail.com>
2025-06-11 20:58:40 -1200

    Add files via upload

---
+++ program1.4.py
@@ -0,0 +1,9 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+temp=a
+a=b
+b=temp
+print("after swapping of two numbers are:")
+print("----------------------")
+print("first value is:",a)
+print("second value is:",b)
