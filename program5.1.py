d00623e84763b94646e28d5a2c88dfdc180fa9d1
python9440 <kummethabharathi93@gmail.com>
2025-06-11 21:08:15 -1200

    Add files via upload

---
+++ program5.1.py
@@ -0,0 +1,11 @@
+class car:
+    def-init-(self,brand,speed):
+        self.brand=brand
+        self.speed=speed
+        def display(self):
+            print(f"brand:{self.brand},speed:{self.speed}km/h")
+            def create_car():
+                car1=car("toyota",180)
+                return car1
+            my_car=create_car()
+            my_car.display()
