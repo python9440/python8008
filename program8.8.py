8b3b05b79c8dae484c30636eaeb5054e7d2edc3d
python9440 <kummethabharathi93@gmail.com>
2025-06-11 21:18:04 -1200

    Add files via upload

---
+++ program8.8.py
@@ -0,0 +1,7 @@
+import platform
+system_name=platform.system()
+node_name=platform.node()
+processor_name=platform.processor()
+print("system name:",system_name)
+print("node name:",node_name)
+print("processor name:",processor_name)
