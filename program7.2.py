1d3b958f9bd64bd769c5f7b42b769e25205eb662
python9440 <kummethabharathi93@gmail.com>
2025-06-11 21:13:06 -1200

    Add files via upload

---
+++ program7.2.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+n=int(input("enter any number:"))
+print("factorial value of",n,"is:",factorial(n))
